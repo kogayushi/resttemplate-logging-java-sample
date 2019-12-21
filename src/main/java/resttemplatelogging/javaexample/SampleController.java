ff50af2d9e992c3a2cfb0a43a5d9aad2ddaede3a
yushi.koga <youshe.old.pleasure@gmail.com>
2019-12-21 22:09:00 +0900

    generate full constructor with lombok

--- src/main/java/resttemplatelogging/javaexample/SampleController.java
+++ src/main/java/resttemplatelogging/javaexample/SampleController.java
@@ -1,16 +1,14 @@
 package resttemplatelogging.javaexample;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
 @RestController
 public class SampleController {
     private final RestTemplate restTemplate;
-
-    public SampleController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     @GetMapping("/")
     public String index() {
